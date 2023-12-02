3d37f146574ad7af5c1f62855d43e681041d9136
Sergio <sl.doncel@protonmail.com>
2023-12-03 00:19:50 +0100

    feat: updating setup.sh for dev environment

--- dependencies/setup.sh
+++ dependencies/setup.sh
@@ -8,4 +8,13 @@ do
   sudo apt install -y ros-noetic-"${p//_/-}"
 done < ros_pkg_list.txt
 
+user_name=$(whoami)
+if [ "$user_name" != "husarion" ]; then
+  # For dev environment, next libraries are neccesary to achieve a correct compilation
+  sudo apt install ros-noetic-libuvc-camera
+  sudo apt install ros-noetic-libuvc-camera-dbgsym
+  sudo apt install ros-noetic-libuvc-ros
+  sudo apt install libgoogle-glog-dev
+fi
+
 printf 'Setup process finished OK :D\n\n'
