4f4f4ab7259a73e443e4d5a0cbe222afc285e445
Sergio <sl.doncel@protonmail.com>
2023-12-02 12:10:10 +0100

    feature: adding setup script to scan dependencies in OS

--- dependencies/setup.sh
+++ dependencies/setup.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+while IFS="" read -r p || [ -n "$p" ]
+do
+  printf 'Installing %s dependency ...\n' "$p"
+  sudo apt install ros-noetic-"${p//_/-}"
+done < ros_pkg_list.txt
+
+printf 'Setup process finished OK :D"
