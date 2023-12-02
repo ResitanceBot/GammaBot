821a9a0d2a6de94e687d13af0fae6862444f3842
Sergio <sl.doncel@protonmail.com>
2023-12-03 00:04:19 +0100

    bugfix: fixing unexpected eof error in setup.sh

--- dependencies/setup.sh
+++ dependencies/setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+printf 'Starting scanning of dependencies, it could take some time ...\n\n'
+
 while IFS="" read -r p || [ -n "$p" ]
 do
   printf 'Installing %s dependency ...\n' "$p"
-  sudo apt install ros-noetic-"${p//_/-}"
+  sudo apt install -y ros-noetic-"${p//_/-}"
 done < ros_pkg_list.txt
 
-printf 'Setup process finished OK :D"
+printf 'Setup process finished OK :D\n\n'
