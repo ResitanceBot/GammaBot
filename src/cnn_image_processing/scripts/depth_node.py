351a22035be0625a76be0796e8c1af3077af10fc
Sergio <sl.doncel@pm.me>
2024-01-08 00:42:23 +0100

    perf: distance calculus in depth node working

--- src/cnn_image_processing/scripts/depth_node.py
+++ src/cnn_image_processing/scripts/depth_node.py
@@ -12,6 +12,7 @@ class Nodo(object):
     def __init__(self):
         # Params
         self.image = None
+        self.aruco = None
         self.br = CvBridge()
         # Node cycle rate (in Hz).
         self.loop_rate = rospy.Rate(10)
@@ -28,20 +29,32 @@ class Nodo(object):
         self.image = self.br.imgmsg_to_cv2(msg, 'passthrough')  # Convierte a escala de grises directamente
         
     def callback_aruco(self, msg):
-        print('Aruco ID:',msg.aruco_id)
+        # print('Aruco ID:',msg.aruco_id)
+        self.aruco = msg
 
     def start(self):
         rospy.loginfo("Starting depth node")
-        image_processed = self.image
         
         while not rospy.is_shutdown():
-            if image_processed is not None:
-                # Muestra la imagen procesada de profundidad
-                self.pub.publish(self.br.cv2_to_imgmsg(image_processed, encoding='16UC1'))
-               
+            if self.image is not None:
+                # rospy.loginfo('-------------------')
+                # self.pub.publish(self.br.cv2_to_imgmsg(image_depth, encoding='16UC1'))
+                if self.aruco is not None:
+                    x = int(self.aruco.corner.x)
+                    y = int(self.aruco.corner.y)
+                    width = int(self.aruco.width)
+                    height = int(self.aruco.height)
+                    image_depth = self.image.copy()
+                    area_aruco = image_depth[y:y+height, x:x+width]
+                    dist_media_aruco = np.mean(area_aruco)
+                    print('Aruco ID:', self.aruco.aruco_id)
+                    print('Distancia (cm):', int(dist_media_aruco/10))
+                    self.aruco = None
+            
             self.loop_rate.sleep()
             
 if __name__ == '__main__':
+    
     rospy.init_node("depth_node", anonymous=True)
     my_node = Nodo()
     my_node.start()
